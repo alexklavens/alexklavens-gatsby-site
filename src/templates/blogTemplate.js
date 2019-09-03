e3eb7a365f4ea199900819980e4cd7c633bd5f1c
Alexander Klavens <alexklavens@Alexanders-MacBook-Pro.local>
2019-09-03 17:04:06 -0400

    update articles

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,6 +3,8 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import styles from "../styles/article.module.css"
 
+// modified example code from gatsby website
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
